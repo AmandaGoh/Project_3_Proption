8e6024cf8dcd409d5ab99e2a246b8c5c8cc6ae8d
Julian Chung <juliancwt77@gmail.com>
2016-11-10 02:23:32 +0800

    add profile page

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
 
   resources :properties
 
+  resources :users, only: [:index, :show, :edit, :update]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
