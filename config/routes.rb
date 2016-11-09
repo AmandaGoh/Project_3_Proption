488e29878e6f537ee5bb04ce135f2558fa05232e
Braden CT Tan <tct13@yahoo.com>
2016-11-09 16:58:15 +0800

    initial files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'home#index'
+ devise_for :users
+ root 'home#index'
 
-  get 'about', to: 'home#about'
+ get 'about', to: 'home#about'
 
-  get 'contact', to: 'home#contact'
+ get 'contact', to: 'home#contact'
 
-  resources :users
 
-  resources :properties
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ resources :properties
+
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
