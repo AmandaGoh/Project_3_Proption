1d6d6e6b6c564170bb64a608d1e073dcef2dced1
Braden CT Tan <tct13@yahoo.com>
2016-11-10 17:37:18 +0800

    created routing for bidding page

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -1,6 +1,14 @@
 class BidsController < ApplicationController
   def index
-    @bids = current_user.bid
+    @bids = current_user.bids
+
+    # current_user.bids.listing.property
+
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @bids }
+    end
   end
 
   def new
