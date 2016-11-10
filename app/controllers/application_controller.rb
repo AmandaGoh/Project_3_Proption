e2f3d75117a0855ae4e4969fe9f8a81f7521706d
Julian Chung <juliancwt77@gmail.com>
2016-11-10 22:50:50 +0800

    finalized profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+  before_action :configure_account_update_params, if: :devise_controller?
+
+protected
+
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :contact_number, :email, :password, :password_confirmation])
+  end
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -46,7 +46,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :contact_number])
   # end
 
   # The path used after sign up.
@@ -64,5 +64,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :contact_number, :email, :password, :password_confirmation])
   end
+
+  
+
 
 end
