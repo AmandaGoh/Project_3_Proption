4a90b21aea3075f0b3b52638d4f3bec08c936b9f
Amanda Oh <[DAmandaGoh>
2016-11-11 15:12:53 +0800

    updated bid table schema to bidderid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,13 +14,13 @@ protected
 
 
 #redirect user to home page after sign up/log in
-  def after_sign_in_path_for(resource)
-    sign_in_url = new_user_session_url
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
-  end
+  # def after_sign_in_path_for(resource)
+  #   sign_in_url = new_user_session_url
+  #   if request.referer == sign_in_url
+  #     super
+  #   else
+  #     stored_location_for(resource) || request.referer || root_path
+  #   end
+  # end
 
 end
