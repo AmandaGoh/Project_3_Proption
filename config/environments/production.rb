9b793fbc51c2e9f3222820ec8840c1da9ebe3ee1
Amanda Oh <[DAmandaGoh>
2016-11-14 11:55:06 +0800

    configured action cables for heroku deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #passing ActionCable's server configuration as an array for heroku deployment
+  config.action_cable.allowed_request_origins = ['https://action-cable-example.herokuapp.com', 'http://action-cable-example.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
