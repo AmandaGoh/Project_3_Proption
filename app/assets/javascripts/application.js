98638968f8816f7c94beef7437e9ce472e400337
Amanda Oh <[DAmandaGoh>
2016-11-16 15:03:58 +0800

    remove bootstrap js from layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,8 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require bootstrap-sprockets
+//= require_tree .
 
 $( document ).on('turbolinks:load', function() {
 
