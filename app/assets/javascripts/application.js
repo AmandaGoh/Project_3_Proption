d6be8f41c8c98ba32e523680e8afde2c19052412
Amanda Oh <[DAmandaGoh>
2016-11-16 14:42:40 +0800

    removed flash remove effect causing jquery error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -49,7 +49,7 @@ $(".alertMessage").show()
 // setTimeout(function() { $(".alertMessage").hide(); }, 1500);
 setTimeout(function() {
   // TODO: Fix this code below
-  $(".alertMessage").fadeOut(5000).remove(5000)
+  $(".alertMessage").fadeOut(5000)
 })
   // $('.message').show('fast').delay(2000).hide('fast')
   // $('.message').remove();
