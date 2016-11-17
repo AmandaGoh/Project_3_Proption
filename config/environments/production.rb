986e8002d083bfcbab96fa15702e589d0025b8c3
Amanda Oh <[DAmandaGoh>
2016-11-17 15:04:50 +0800

    uncomment action cables links in production rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,10 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # #passing ActionCable's server configuration as an array for heroku deployment
-  # config.action_cable.url = 'wss://proption.herokuapp.com/cable'
-  # config.action_cable.allowed_request_origins = ['https://proption.herokuapp.com', 'http://proption.herokuapp.com']
-  #
+  #passing ActionCable's server configuration as an array for heroku deployment
+  config.action_cable.url = 'wss://proption.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = ['https://proption.herokuapp.com', 'http://proption.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
